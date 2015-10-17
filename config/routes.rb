3e0d45539ac02dd00d9fc2ad9ccfbc14184e4f83
oriken111 <oriken111@gmail.com>
2015-10-17 18:11:04 +0900

    for restarting everything

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'hello#index'
+  get 'hello/index' => 'hello#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
@@ -39,7 +39,6 @@ Rails.application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
